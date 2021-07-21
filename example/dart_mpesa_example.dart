b26c639ca33a7c55bd9b6cec3bacb89f142245ac
Crazy Genius <elyudde@gmail.com>
2021-07-21 23:35:16 +0300

    initial commit

---
+++ example/dart_mpesa_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_mpesa/dart_mpesa.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_mpesa.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_mpesa;
+
+
+class Mpesa {
+  
+}
