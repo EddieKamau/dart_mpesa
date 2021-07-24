59c6ab7851abfc4456977aa6ac247703378cf24f
Crazy Genius <elyudde@gmail.com>
2021-07-24 17:48:20 +0300

    mpesa setup

--- lib/dart_mpesa.dart
+++ lib/dart_mpesa.dart
@@ -5,6 +5,25 @@ library dart_mpesa;
 
 
 class Mpesa {
+  Mpesa({
+    required this.shortCode,
+    required this.consumerKey,
+    required this.consumerSecret,
+    this.initiatorName,
+    this.securityCredential,
+    this.passKey
+  });
+
+  ApplicationMode applicationMode = ApplicationMode.production;
+
+  String shortCode;
+  String consumerSecret;
+  String consumerKey;
+
+  String? initiatorName;
+  String? securityCredential;
+  
+  String? passKey;
   
 }
 
