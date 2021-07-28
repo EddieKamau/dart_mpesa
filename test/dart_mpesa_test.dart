6e360ee58973923536da579faf19a25983baa0a8
Crazy Genius <elyudde@gmail.com>
2021-07-28 16:13:02 +0300

    dartfmt -w .

--- test/dart_mpesa_test.dart
+++ test/dart_mpesa_test.dart
@@ -11,50 +11,81 @@ class MockMpesa extends Mock implements Mpesa {
   @override
   IdentifierType get identifierType => identifierT;
   @override
-  Future<MpesaResponse> accountBalance({required String remarks, required String queueTimeOutURL, required String resultURL}) {
+  Future<MpesaResponse> accountBalance(
+      {required String remarks,
+      required String queueTimeOutURL,
+      required String resultURL}) {
     return Future.value(MpesaResponse.fromMap(200, default1));
   }
 
   @override
   Future<MpesaResponse> b2cTransaction({
-    required String phoneNumber, required double amount, required String remarks,
-    required String occassion, required String queueTimeOutURL, required String resultURL,
-    BcCommandId commandID = BcCommandId.BusinessPayment,}) {
+    required String phoneNumber,
+    required double amount,
+    required String remarks,
+    required String occassion,
+    required String queueTimeOutURL,
+    required String resultURL,
+    BcCommandId commandID = BcCommandId.BusinessPayment,
+  }) {
     return Future.value(MpesaResponse.fromMap(200, default1));
   }
 
   @override
-  Future<MpesaResponse> b2bBuyGoodsTransaction({required String shortCode, required double amount, required String remarks, required String queueTimeOutURL, required String resultURL}) {
+  Future<MpesaResponse> b2bBuyGoodsTransaction(
+      {required String shortCode,
+      required double amount,
+      required String remarks,
+      required String queueTimeOutURL,
+      required String resultURL}) {
     return Future.value(MpesaResponse.fromMap(200, default1));
   }
 
   @override
-  Future<MpesaResponse> b2bPaybillTransaction({required String shortCode, required double amount, required String remarks, required String accountReference, required String queueTimeOutURL, required String resultURL}) {
+  Future<MpesaResponse> b2bPaybillTransaction(
+      {required String shortCode,
+      required double amount,
+      required String remarks,
+      required String accountReference,
+      required String queueTimeOutURL,
+      required String resultURL}) {
     return Future.value(MpesaResponse.fromMap(200, default1));
   }
 
   @override
-  Future<MpesaResponse> b2bTransaction({required String shortCode, required double amount, required String remarks, required String queueTimeOutURL, required String resultURL, required IdentifierType identifierType, required BbCommandId commandID, String? accountReference}) {
-    if(commandID == BbCommandId.MerchantToMerchantTransfer){
-      if(this.identifierType == IdentifierType.OrganizationShortCode){
-
-        if(identifierType == IdentifierType.OrganizationShortCode){
+  Future<MpesaResponse> b2bTransaction(
+      {required String shortCode,
+      required double amount,
+      required String remarks,
+      required String queueTimeOutURL,
+      required String resultURL,
+      required IdentifierType identifierType,
+      required BbCommandId commandID,
+      String? accountReference}) {
+    if (commandID == BbCommandId.MerchantToMerchantTransfer) {
+      if (this.identifierType == IdentifierType.OrganizationShortCode) {
+        if (identifierType == IdentifierType.OrganizationShortCode) {
           return Future.value(MpesaResponse.fromMap(200, default1));
-        }else{
-          return Future.value(MpesaResponse.fromMap(400, b2bReceiverError(identifierType.value)));
+        } else {
+          return Future.value(MpesaResponse.fromMap(
+              400, b2bReceiverError(identifierType.value)));
         }
-      }else{
-        return Future.value(MpesaResponse.fromMap(400, b2bPrimaryError(this.identifierType.value)));
+      } else {
+        return Future.value(MpesaResponse.fromMap(
+            400, b2bPrimaryError(this.identifierType.value)));
       }
-
-    }else{
+    } else {
       return Future.value(MpesaResponse.fromMap(200, default1));
     }
-    
   }
 
   @override
-  Future<MpesaResponse> lipanaMpesaOnline({required String phoneNumber, required double amount, required String accountReference, required String transactionDesc, required String callBackURL}) {
+  Future<MpesaResponse> lipanaMpesaOnline(
+      {required String phoneNumber,
+      required double amount,
+      required String accountReference,
+      required String transactionDesc,
+      required String callBackURL}) {
     return Future.value(MpesaResponse.fromMap(200, stkSuccess));
   }
 
@@ -64,17 +95,26 @@ class MockMpesa extends Mock implements Mpesa {
   }
 
   @override
-  Future<MpesaResponse> reversalTransaction({required String transactionID, required double amount, required String remarks, required String occassion, required String queueTimeOutURL, required String resultURL}) {
+  Future<MpesaResponse> reversalTransaction(
+      {required String transactionID,
+      required double amount,
+      required String remarks,
+      required String occassion,
+      required String queueTimeOutURL,
+      required String resultURL}) {
     return Future.value(MpesaResponse.fromMap(200, default1));
   }
 
   @override
-  Future<MpesaResponse> transactionStatus({required String transactionID, required IdentifierType identifierType, required String remarks, required String occassion, required String queueTimeOutURL, required String resultURL}) {
+  Future<MpesaResponse> transactionStatus(
+      {required String transactionID,
+      required IdentifierType identifierType,
+      required String remarks,
+      required String occassion,
+      required String queueTimeOutURL,
+      required String resultURL}) {
     return Future.value(MpesaResponse.fromMap(200, default1));
   }
-
-  
-  
 }
 
 void main() {
@@ -85,34 +125,52 @@ void main() {
       // Additional setup goes here.
     });
 
-    test('b2b', ()async {
+    test('b2b', () async {
       expect(
-        (await mpesa.b2bTransaction(shortCode: "", amount: 0, remarks: '', queueTimeOutURL: '', resultURL: '', identifierType: IdentifierType.OrganizationShortCode, commandID: BbCommandId.BusinessToBusinessTransfer)).statusCode, 
-        200
-      );
+          (await mpesa.b2bTransaction(
+                  shortCode: "",
+                  amount: 0,
+                  remarks: '',
+                  queueTimeOutURL: '',
+                  resultURL: '',
+                  identifierType: IdentifierType.OrganizationShortCode,
+                  commandID: BbCommandId.BusinessToBusinessTransfer))
+              .statusCode,
+          200);
     });
 
-    test('lipa na mpesa', ()async {
+    test('lipa na mpesa', () async {
       expect(
-        (await mpesa.lipanaMpesaOnline(phoneNumber: "", amount: 0, accountReference: "", transactionDesc: "", callBackURL: "callBackURL")).merchantRequestID, 
-        "29115-34620561-1"
-      );
+          (await mpesa.lipanaMpesaOnline(
+                  phoneNumber: "",
+                  amount: 0,
+                  accountReference: "",
+                  transactionDesc: "",
+                  callBackURL: "callBackURL"))
+              .merchantRequestID,
+          "29115-34620561-1");
     });
 
-    test('account balance', ()async {
+    test('account balance', () async {
       expect(
-        (await mpesa.accountBalance(remarks: "remarks", queueTimeOutURL: "", resultURL: "")).conversationID, 
-        "AG_20191219_00005797af5d7d75f652"
-      );
+          (await mpesa.accountBalance(
+                  remarks: "remarks", queueTimeOutURL: "", resultURL: ""))
+              .conversationID,
+          "AG_20191219_00005797af5d7d75f652");
     });
 
-    test('b2b error', ()async {
+    test('b2b error', () async {
       expect(
-        (await mpesa.b2bTransaction(shortCode: "", amount: 0, remarks: '', queueTimeOutURL: '', resultURL: '', identifierType: IdentifierType.TillNumber, commandID: BbCommandId.MerchantToMerchantTransfer)).statusCode, 
-        400
-      );
+          (await mpesa.b2bTransaction(
+                  shortCode: "",
+                  amount: 0,
+                  remarks: '',
+                  queueTimeOutURL: '',
+                  resultURL: '',
+                  identifierType: IdentifierType.TillNumber,
+                  commandID: BbCommandId.MerchantToMerchantTransfer))
+              .statusCode,
+          400);
     });
-
-    
   });
 }
--- test/responses.dart
+++ test/responses.dart
@@ -1,132 +1,97 @@
 // stkpush
 // success
-Map<String, dynamic> stkSuccess = {    
-   "MerchantRequestID": "29115-34620561-1",    
-   "CheckoutRequestID": "ws_CO_191220191020363925",    
-   "ResponseCode": "0",    
-   "ResponseDescription": "Success. Request accepted for processing",    
-   "CustomerMessage": "Success. Request accepted for processing"
+Map<String, dynamic> stkSuccess = {
+  "MerchantRequestID": "29115-34620561-1",
+  "CheckoutRequestID": "ws_CO_191220191020363925",
+  "ResponseCode": "0",
+  "ResponseDescription": "Success. Request accepted for processing",
+  "CustomerMessage": "Success. Request accepted for processing"
 };
 // successfull callback
-Map<String, dynamic> stkSuccessCallBack = {    
-   "Body": {        
-      "stkCallback": {            
-         "MerchantRequestID": "29115-34620561-1",            
-         "CheckoutRequestID": "ws_CO_191220191020363925",            
-         "ResultCode": 0,            
-         "ResultDesc": "The service request is processed successfully.",            
-         "CallbackMetadata": {                
-            "Item": [{                        
-               "Name": "Amount",                        
-               "Value": 1.00                    
-            },                    
-            {                        
-               "Name": "MpesaReceiptNumber",                        
-               "Value": "NLJ7RT61SV"                    
-            },                    
-            {                        
-               "Name": "TransactionDate",                        
-               "Value": 20191219102115                    
-            },                    
-            {                        
-               "Name": "PhoneNumber",                        
-               "Value": 254708374149                    
-            }]            
-         }        
-      }    
-   }
+Map<String, dynamic> stkSuccessCallBack = {
+  "Body": {
+    "stkCallback": {
+      "MerchantRequestID": "29115-34620561-1",
+      "CheckoutRequestID": "ws_CO_191220191020363925",
+      "ResultCode": 0,
+      "ResultDesc": "The service request is processed successfully.",
+      "CallbackMetadata": {
+        "Item": [
+          {"Name": "Amount", "Value": 1.00},
+          {"Name": "MpesaReceiptNumber", "Value": "NLJ7RT61SV"},
+          {"Name": "TransactionDate", "Value": 20191219102115},
+          {"Name": "PhoneNumber", "Value": 254708374149}
+        ]
+      }
+    }
+  }
 };
 
 // failed callback
-Map<String, dynamic> stkFailedCallback = {    
-   "Body": {
-      "stkCallback": {
-         "MerchantRequestID": "29115-34620561-1",
-         "CheckoutRequestID": "ws_CO_191220191020363925",
-         "ResultCode": 1032,
-         "ResultDesc": "Request cancelled by user."
-      }
-   }
+Map<String, dynamic> stkFailedCallback = {
+  "Body": {
+    "stkCallback": {
+      "MerchantRequestID": "29115-34620561-1",
+      "CheckoutRequestID": "ws_CO_191220191020363925",
+      "ResultCode": 1032,
+      "ResultDesc": "Request cancelled by user."
+    }
+  }
 };
-
 
 // c2b
-// validation 
+// validation
 Map<String, dynamic> c2bValidation = {
-   "TransactionType":"Pay Bill",
-   "TransID":"RKTQDM7W6S",
-   "TransTime":"20191122063845",
-   "TransAmount":"10",
-   "BusinessShortCode":"600638",
-   "BillRefNumber":"254708374149",
-   "InvoiceNumber":"",
-   "OrgAccountBalance":"49197.00",
-   "ThirdPartyTransID":"",
-   "MSISDN":"254708374149",
-   "FirstName":"John",
-   "MiddleName":"",
-   "LastName":"Doe"
+  "TransactionType": "Pay Bill",
+  "TransID": "RKTQDM7W6S",
+  "TransTime": "20191122063845",
+  "TransAmount": "10",
+  "BusinessShortCode": "600638",
+  "BillRefNumber": "254708374149",
+  "InvoiceNumber": "",
+  "OrgAccountBalance": "49197.00",
+  "ThirdPartyTransID": "",
+  "MSISDN": "254708374149",
+  "FirstName": "John",
+  "MiddleName": "",
+  "LastName": "Doe"
 };
-
 
 // b2c
 // success
-Map<String, dynamic> b2cSuccesCallback = {    
-   "Result": {
-      "ResultType": 0,
-      "ResultCode": 0,
-      "ResultDesc": "The service request is processed successfully.", 
-      "OriginatorConversationID": "10571-7910404-1",
-      "ConversationID": "AG_20191219_00004e48cf7e3533f581",
-      "TransactionID": "NLJ41HAY6Q",
-      "ResultParameters": {
-         "ResultParameter": [
-          {
-             "Key": "TransactionAmount",
-             "Value": 10
-          },
-          {
-             "Key": "TransactionReceipt",
-             "Value": "NLJ41HAY6Q"
-          },
-          {
-             "Key": "B2CRecipientIsRegisteredCustomer",
-             "Value": "Y"
-          },
-          {
-             "Key": "B2CChargesPaidAccountAvailableFunds",
-             "Value": -4510.00
-          },
-          {
-             "Key": "ReceiverPartyPublicName",
-             "Value": "254708374149 - John Doe"
-          },
-          {
-             "Key": "TransactionCompletedDateTime",
-             "Value": "19.12.2019 11:45:50"
-          },
-          {
-             "Key": "B2CUtilityAccountAvailableFunds",
-             "Value": 10116.00
-          },
-          {
-             "Key": "B2CWorkingAccountAvailableFunds",
-             "Value": 900000.00
-          }
-        ]
-      },
-      "ReferenceData": {
-         "ReferenceItem": {
-            "Key": "QueueTimeoutURL",
-            "Value": "https:\/\/internalsandbox.safaricom.co.ke\/mpesa\/b2cresults\/v1\/submit"
-          }
+Map<String, dynamic> b2cSuccesCallback = {
+  "Result": {
+    "ResultType": 0,
+    "ResultCode": 0,
+    "ResultDesc": "The service request is processed successfully.",
+    "OriginatorConversationID": "10571-7910404-1",
+    "ConversationID": "AG_20191219_00004e48cf7e3533f581",
+    "TransactionID": "NLJ41HAY6Q",
+    "ResultParameters": {
+      "ResultParameter": [
+        {"Key": "TransactionAmount", "Value": 10},
+        {"Key": "TransactionReceipt", "Value": "NLJ41HAY6Q"},
+        {"Key": "B2CRecipientIsRegisteredCustomer", "Value": "Y"},
+        {"Key": "B2CChargesPaidAccountAvailableFunds", "Value": -4510.00},
+        {"Key": "ReceiverPartyPublicName", "Value": "254708374149 - John Doe"},
+        {"Key": "TransactionCompletedDateTime", "Value": "19.12.2019 11:45:50"},
+        {"Key": "B2CUtilityAccountAvailableFunds", "Value": 10116.00},
+        {"Key": "B2CWorkingAccountAvailableFunds", "Value": 900000.00}
+      ]
+    },
+    "ReferenceData": {
+      "ReferenceItem": {
+        "Key": "QueueTimeoutURL",
+        "Value":
+            "https:\/\/internalsandbox.safaricom.co.ke\/mpesa\/b2cresults\/v1\/submit"
       }
-   }
+    }
+  }
 };
 
 // failed
 Map<String, dynamic> b2cFailedCallback = {
- "Result": {
+  "Result": {
     "ResultType": 0,
     "ResultCode": 2001,
     "ResultDesc": "The initiator information is invalid.",
@@ -135,94 +100,77 @@ Map<String, dynamic> b2cFailedCallback = {
     "TransactionID": "NLJ0000000",
     "ReferenceData": {
       "ReferenceItem": {
-          "Key": "QueueTimeoutURL",
-          "Value": "https:\/\/internalsandbox.safaricom.co.ke\/mpesa\/b2cresults\/v1\/submit"
-        }
+        "Key": "QueueTimeoutURL",
+        "Value":
+            "https:\/\/internalsandbox.safaricom.co.ke\/mpesa\/b2cresults\/v1\/submit"
+      }
     }
- }
+  }
 };
 // error
-Map<String, dynamic> b2cError = {    
-   "requestId": "11728-2929992-1",
-   "errorCode": "401.002.01",
-   "errorMessage": "Error Occurred - Invalid Access Token - BJGFGOXv5aZnw90KkA4TDtu4Xdyf"
+Map<String, dynamic> b2cError = {
+  "requestId": "11728-2929992-1",
+  "errorCode": "401.002.01",
+  "errorMessage":
+      "Error Occurred - Invalid Access Token - BJGFGOXv5aZnw90KkA4TDtu4Xdyf"
 };
 
 // accept
-Map<String, dynamic> default1 = {    
- "ConversationID": "AG_20191219_00005797af5d7d75f652",    
- "OriginatorConversationID": "16740-34861180-1",    
- "ResponseCode": "0",    
- "ResponseDescription": "Accept the service request successfully."
+Map<String, dynamic> default1 = {
+  "ConversationID": "AG_20191219_00005797af5d7d75f652",
+  "OriginatorConversationID": "16740-34861180-1",
+  "ResponseCode": "0",
+  "ResponseDescription": "Accept the service request successfully."
 };
 
 // reversal
 // succes
 Map<String, dynamic> reversalSuccess = {
-"OriginatorConversationID": "71840-27539181-07",
-"ConversationID": "AG_20210709_12346c8e6f8858d7b70a",
-"ResponseCode":"0",
-"ResponseDescription": "Accept the service request successfully."
-
+  "OriginatorConversationID": "71840-27539181-07",
+  "ConversationID": "AG_20210709_12346c8e6f8858d7b70a",
+  "ResponseCode": "0",
+  "ResponseDescription": "Accept the service request successfully."
 };
 
 // callback
 Map<String, dynamic> reversalCallbackSuccess = {
-"Result": {
-  "ResultType": 0,
-  "ResultCode": 21,
-  "ResultDesc": "The service request is processed successfully",
-  "OriginatorConversationID": "8521-4298025-1",
-  "ConversationID": "AG_20181005_00004d7ee675c0c7ee0b",
-  "TransactionID": "MJ561H6X5O",
-  "ResultParameters": {
-  "ResultParameter": [
-      {
-      "Key": "DebitAccountBalance",
-      "Value": "Utility Account|KES|51661.00|51661.00|0.00|0.00"
-      }, 
-      {
-      "Key": "Amount",
-      "Value": 100
-      }, 
-      {
-      "Key": "TransCompletedTime",
-      "Value": 20181005153225
-      }, 
-      {
-      "Key": "OriginalTransactionID",
-      "Value": "MJ551H6X5D"
-      }, 
-      {
-      "Key": "Charge",
-      "Value": 0
-      }, 
-      {
-      "Key": "CreditPartyPublicName",
-      "Value": "254708374149 - John Doe"
-      }, 
-      {
-      "Key": "DebitPartyPublicName",
-      "Value": "601315 - Safaricom1338"
-      }
-    ]
-  },
-  "ReferenceData": {
+  "Result": {
+    "ResultType": 0,
+    "ResultCode": 21,
+    "ResultDesc": "The service request is processed successfully",
+    "OriginatorConversationID": "8521-4298025-1",
+    "ConversationID": "AG_20181005_00004d7ee675c0c7ee0b",
+    "TransactionID": "MJ561H6X5O",
+    "ResultParameters": {
+      "ResultParameter": [
+        {
+          "Key": "DebitAccountBalance",
+          "Value": "Utility Account|KES|51661.00|51661.00|0.00|0.00"
+        },
+        {"Key": "Amount", "Value": 100},
+        {"Key": "TransCompletedTime", "Value": 20181005153225},
+        {"Key": "OriginalTransactionID", "Value": "MJ551H6X5D"},
+        {"Key": "Charge", "Value": 0},
+        {"Key": "CreditPartyPublicName", "Value": "254708374149 - John Doe"},
+        {"Key": "DebitPartyPublicName", "Value": "601315 - Safaricom1338"}
+      ]
+    },
+    "ReferenceData": {
       "ReferenceItem": {
         "Key": "QueueTimeoutURL",
-        "Value": "https://internalsandbox.safaricom.co.ke/mpesa/reversalresults/v1/submit"
-        }
+        "Value":
+            "https://internalsandbox.safaricom.co.ke/mpesa/reversalresults/v1/submit"
       }
     }
+  }
 };
-
 
 // transaction status
 // success
 Map<String, dynamic> transactionSuccess = {
   "OriginatorConversationID": "1236-7134259-1",
   "ConversationID": "AG_20210709_1234409f86436c583e3f",
-  "ResponseCode":"0",
+  "ResponseCode": "0",
   "ResponseDescription": "Accept the service request successfully."
 };
 Map<String, dynamic> transactionSuccessCallback = {
@@ -230,65 +178,25 @@ Map<String, dynamic> transactionSuccessCallback = {
     "ConversationID": "AG_20180223_0000493344ae97d86f75",
     "OriginatorConversationID": "3213-416199-2",
     "ReferenceData": {
-      "ReferenceItem": {
-        "Key": "Occasion"
-      }
+      "ReferenceItem": {"Key": "Occasion"}
     },
     "ResultCode": 0,
     "ResultDesc": "The service request is processed successfully.",
     "ResultParameters": {
       "ResultParameter": [
-        {
-          "Key": "DebitPartyName",
-          "Value": "600310 - Safaricom333"
-        },
-        {
-          "Key": "CreditPartyName",
-          "Value": "254708374149 - John Doe"
-        },
-        {
-          "Key": "OriginatorConversationID",
-          "Value": "3211-416020-3"
-        },
-        {
-          "Key": "InitiatedTime",
-          "Value": 20180223054112
-        },
-        {
-          "Key": "DebitAccountType",
-          "Value": "Utility Account"
-        },
-        {
-          "Key": "DebitPartyCharges",
-          "Value": "Fee For B2C Payment|KES|22.40"
-        },
-        {
-          "Key": "TransactionReason"
-        },
-        {
-          "Key": "ReasonType",
-          "Value": "Business Payment to Customer via API"
-        },
-        {
-          "Key": "TransactionStatus",
-          "Value": "Completed"
-        },
-        {
-          "Key": "FinalisedTime",
-          "Value": 20180223054112
-        },
-        {
-          "Key": "Amount",
-          "Value": 300
-        },
-        {
-          "Key": "ConversationID",
-          "Value": "AG_20180223_000041b09c22e613d6c9"
-        },
-        {
-          "Key": "ReceiptNo",
-          "Value": "MBN31H462N"
-        }
+        {"Key": "DebitPartyName", "Value": "600310 - Safaricom333"},
+        {"Key": "CreditPartyName", "Value": "254708374149 - John Doe"},
+        {"Key": "OriginatorConversationID", "Value": "3211-416020-3"},
+        {"Key": "InitiatedTime", "Value": 20180223054112},
+        {"Key": "DebitAccountType", "Value": "Utility Account"},
+        {"Key": "DebitPartyCharges", "Value": "Fee For B2C Payment|KES|22.40"},
+        {"Key": "TransactionReason"},
+        {"Key": "ReasonType", "Value": "Business Payment to Customer via API"},
+        {"Key": "TransactionStatus", "Value": "Completed"},
+        {"Key": "FinalisedTime", "Value": 20180223054112},
+        {"Key": "Amount", "Value": 300},
+        {"Key": "ConversationID", "Value": "AG_20180223_000041b09c22e613d6c9"},
+        {"Key": "ReceiptNo", "Value": "MBN31H462N"}
       ]
     },
     "ResultType": 0,
@@ -296,26 +204,27 @@ Map<String, dynamic> transactionSuccessCallback = {
   }
 };
 
-
 // stkQuery
 // success
 Map<String, dynamic> stkQuer = {
   "ResponseCode": "0",
-  "ResponseDescription":"The service request has been accepted successsfully",
-  "MerchantRequestID":"22205-34066-1",
-  "CheckoutRequestID":"ws_CO_13012021093521236557",
+  "ResponseDescription": "The service request has been accepted successsfully",
+  "MerchantRequestID": "22205-34066-1",
+  "CheckoutRequestID": "ws_CO_13012021093521236557",
   "ResultCode": "0",
-  "ResultDesc":"The service request is processed successfully."
+  "ResultDesc": "The service request is processed successfully."
 };
 
-Map<String, dynamic> b2bReceiverError (int value) => {    
-   "requestId": "11728-2929992-1",
-   "errorCode": "401.002.01",
-   "errorMessage": "The element The value of Request.Identity.ReceiverParty.IdentifierType can not be $value is invalid."
-};
+Map<String, dynamic> b2bReceiverError(int value) => {
+      "requestId": "11728-2929992-1",
+      "errorCode": "401.002.01",
+      "errorMessage":
+          "The element The value of Request.Identity.ReceiverParty.IdentifierType can not be $value is invalid."
+    };
 
-Map<String, dynamic> b2bPrimaryError (int value) =>{    
-   "requestId": "11728-2929992-1",
-   "errorCode": "401.002.01",
-   "errorMessage": "The element The value of Request.Identity.PrimaryParty.IdentifierType can not be $value is invalid."
-};
+Map<String, dynamic> b2bPrimaryError(int value) => {
+      "requestId": "11728-2929992-1",
+      "errorCode": "401.002.01",
+      "errorMessage":
+          "The element The value of Request.Identity.PrimaryParty.IdentifierType can not be $value is invalid."
+    };
